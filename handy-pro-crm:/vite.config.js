d34010beaab92e28d2c3687a543d842b19a3979b
handyproservice20-dotcom <handyproservice20@gmail.com>
2026-06-07 02:41:15 -0400

    Delete handy-pro-crm: directory

--- handy-pro-crm:/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// IMPORTANT: Change '/handy-pro-crm/' to match your GitHub repository name
-// Example: if your repo is github.com/yourname/my-crm, use '/my-crm/'
-export default defineConfig({
-  plugins: [react()],
-  base: '/NEW-HANY-PRO-CRM/',
-})
